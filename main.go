50aae78d8ad83c537b1ddde1cbcd4f8348ded864
Vladimir Popov <vladimir.popov@xored.com>
2021-09-24 15:16:06 +0700

    Manually update to the latest sdk version
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- main.go
+++ main.go
@@ -31,7 +31,6 @@ import (
 	"github.com/edwarnicke/grpcfd"
 
 	"github.com/networkservicemesh/sdk/pkg/tools/opentracing"
-	"github.com/networkservicemesh/sdk/pkg/tools/resetting"
 	"github.com/networkservicemesh/sdk/pkg/tools/token"
 
 	registryconnect "github.com/networkservicemesh/sdk/pkg/registry/common/connect"
@@ -129,13 +128,11 @@ func main() {
 			grpc.PerRPCCredentials(token.NewPerRPCCredentials(spiffejwt.TokenGeneratorFunc(source, config.MaxTokenLifetime))),
 		),
 		grpc.WithTransportCredentials(
-			resetting.NewCredentials(
-				grpcfd.TransportCredentials(
-					credentials.NewTLS(
-						tlsconfig.MTLSClientConfig(source, source, tlsconfig.AuthorizeAny()),
-					),
+			grpcfd.TransportCredentials(
+				credentials.NewTLS(
+					tlsconfig.MTLSClientConfig(source, source, tlsconfig.AuthorizeAny()),
 				),
-				source.Updated()),
+			),
 		),
 		grpcfd.WithChainStreamInterceptor(),
 		grpcfd.WithChainUnaryInterceptor(),
